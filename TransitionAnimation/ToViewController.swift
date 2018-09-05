183d634b99e438a69f414a3897f7f609064a7ebd
takehilo <takehilo.kaneko@gmail.com>
2018-09-05 14:12:14 +0900

    元の画面に戻る機能を追加

--- TransitionAnimation/ToViewController.swift
+++ TransitionAnimation/ToViewController.swift
@@ -6,8 +6,21 @@ class ToViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .blue
+
         transitioningDelegate = delegate
 
-        view.backgroundColor = .blue
+        let button = UIButton()
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("元の画面へ", for: .normal)
+        button.addTarget(self, action: #selector(ToViewController.back(_:)), for: .touchUpInside)
+        button.frame.size = CGSize(width: 100, height: 50)
+        button.center = view.center
+
+        view.addSubview(button)
+    }
+
+    @objc func back(_ sender: UIButton?) {
+        dismiss(animated: true, completion: nil)
     }
 }
